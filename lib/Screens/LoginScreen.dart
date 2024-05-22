6cf780c9cc8d4c0e72d4528bc059954d733803e7
luxurate <abdulamreena@gmail.com>
2024-05-22 23:48:51 +0530

    gh

--- lib/Screens/LoginScreen.dart
+++ lib/Screens/LoginScreen.dart
@@ -31,7 +31,7 @@ class _LoginScreenState extends State<LoginScreen> {
             child: Column(
               mainAxisAlignment: MainAxisAlignment.center,
               children: [
-                SizedBox(height: 90),
+                SizedBox(height: 80),
                 TextFormField(
                   controller: _usernameController,
                   decoration: InputDecoration(
